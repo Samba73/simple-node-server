6380c2bf506cbdeb3bb1bcde44dc210af82e09fe
Samba73 <90577515+Samba73@users.noreply.github.com>
2022-05-16 12:15:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (req, res) {
     }
     else if (req.url == "/home") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Home Page from Samba github.Changes made.</p></body></html>');
+        res.write('<html><body><p>This is Home Page from Samba github and viewing through AWS pipeline.</p></body></html>');
         res.end();
 
     }
